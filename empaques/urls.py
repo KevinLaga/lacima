fdbd462cbad9a0fd470d610f2497a8195de7654a
Kevin_Laga <kevinhlaga@gmail.com>
2025-09-24 12:35:51 -0700

    prueba

--- empaques/urls.py
+++ empaques/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path, include
 from . import views
+from django.http import HttpResponse
+from django.db import connection
+from django.conf import settings
+def _dbg_db(request):
+    with connection.cursor() as c:
+        # esto devuelve la ruta REAL del archivo que abrió sqlite
+        c.execute("PRAGMA database_list;")
+        dblist = c.fetchall()
+        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='empaques_productiondisplay';")
+        has = bool(c.fetchone())
+    return HttpResponse(
+        f"settings.DB_NAME={settings.DATABASES['default']['NAME']}<br>"
+        f"PRAGMA database_list={dblist}<br>"
+        f"has_empaques_productiondisplay={has}"
+    )
+
 
 urlpatterns = [
     path('nuevo/', views.shipment_create, name='shipment_create'),
@@ -8,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('reporte/shipment/<int:shipment_id>/', views.daily_report, name='daily_report_by_id'),
     path('reporte/tracking/<str:tracking>/', views.daily_report, name='daily_report_by_tracking'),
+    path("__debug_db__", _dbg_db),
 
     # Producción (las que sí tenemos implementadas)
     path("produccion/", views.production_today, name="production_today"),
